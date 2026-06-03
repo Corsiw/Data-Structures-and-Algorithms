26aa018e483df836b9c4d73b597eb36ccc2c4412
Corsiw <corsiw.dt.7@gmail.com>
2026-06-04 00:03:42 +0300

    Merge P11

--- SET10/P11.cpp
+++ SET10/P11.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 
+bool used[10][10][3];
 
-bool IterateRows(std::vector<std::vector<int> >& matrix, const int max_mask, const int c, const int iter) {
+bool DFS(std::vector<std::vector<int> >& matrix, const int max_mask, const int c, const int iter) {
   if (iter >= matrix.size()) {
     return true;
   }
@@ -15,13 +16,14 @@ bool IterateRows(std::vector<std::vector<int> >& matrix, const int max_mask, con
       mask /= c;
     }
 
-    matrix[iter] = row;
-
     bool check = true;
-    for (int up = 0; up < iter; up++) {
-      for (int left = 0; left < matrix[0].size(); left++) {
-        for (int right = left + 1; right < matrix[0].size(); right++) {
-          if (matrix[up][left] == matrix[up][right] && matrix[up][left] == matrix[iter][left] && matrix[up][right] == matrix[iter][right]) {
+
+    for (int c1 = 0; c1 < matrix[0].size() && check; c1++) {
+      for (int c2 = c1 + 1; c2 < matrix[0].size(); c2++) {
+        if (row[c1] == row[c2]) {
+          int color = row[c1];
+
+          if (used[c1][c2][color]) {
             check = false;
             break;
           }
@@ -29,12 +31,58 @@ bool IterateRows(std::vector<std::vector<int> >& matrix, const int max_mask, con
       }
     }
 
-    if (check && IterateRows(matrix, max_mask, c, iter + 1)) {
+    if (!check) {
+      continue;
+    }
+
+    std::vector<std::tuple<int, int, int>> added;
+
+    for (int c1 = 0; c1 < matrix[0].size(); ++c1) {
+      for (int c2 = c1 + 1; c2 < matrix[0].size(); ++c2) {
+        if (row[c1] == row[c2]) {
+          const int color = row[c1];
+
+          if (!used[c1][c2][color]) {
+            used[c1][c2][color] = true;
+            added.emplace_back(c1, c2, color);
+          }
+        }
+      }
+    }
+
+    matrix[iter] = row;
+
+    if (DFS(matrix, max_mask, c, iter + 1)) {
       return true;
+    }
+
+    for (auto [c1, c2, color] : added) {
+      used[c1][c2][color] = false;
     }
   }
 
   return false;
+}
+
+void GetOptimalC3(std::vector<std::vector<int> >& matrix, const int n, const int m) {
+  const int sol[10][10] = {
+    {1,2,1,2,0,1,0,1,2,0},
+    {2,1,0,2,2,0,0,1,0,1},
+    {2,2,1,1,0,2,1,0,0,2},
+    {2,0,0,0,0,1,1,2,2,1},
+    {0,0,2,2,1,2,1,1,0,0},
+    {2,0,2,1,1,0,2,0,1,1},
+    {0,1,1,2,0,0,2,2,1,2},
+    {0,1,2,0,1,1,0,0,2,2},
+    {1,1,0,1,2,2,2,0,2,0},
+    {1,2,2,0,2,0,1,2,1,0}
+  };
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      matrix[i][j] = sol[i][j];
+    }
+  }
 }
 
 int main() {
@@ -46,13 +94,19 @@ int main() {
   int c = 0;
   std::cin >> n >> m >> c;
 
-  int max_mask = 1;
-  for (int i = 0; i < m; i++) {
-    max_mask *= c;
+  std::vector matrix(n, std::vector<int>(m));
+
+  if (c == 3) {
+    GetOptimalC3(matrix, n, m);
+  } else {
+    int max_mask = 1;
+    for (int i = 0; i < m; i++) {
+      max_mask *= c;
+    }
+
+    DFS(matrix, max_mask, c, 0);
   }
 
-  std::vector matrix(n ,std::vector<int>(m));
-  IterateRows(matrix, max_mask, c, 0);
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
       std::cout << matrix[i][j] + 1 << " ";
